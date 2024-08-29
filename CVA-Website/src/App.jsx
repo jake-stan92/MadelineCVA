6da61f70b0d2055ea5085bb0b265a34a2d171b12
Jake Stanier <jake.stanier@hotmail.co.uk>
2024-08-29 16:39:42 +0100

    home page initial layout added

--- CVA-Website/src/App.jsx
+++ CVA-Website/src/App.jsx
@@ -1,12 +1,40 @@
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Reviews from "./components/Reviews";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <div className="section">
+        <h3>
+          CONTENT DESIGNED WITH DEEPER INTENTIONS AND INSPIRED BY YOUR UNIQUE
+          BUSINESS
+        </h3>
+        <p>img here</p>
+        <button>
+          <a href="/services">services</a>
+        </button>
+      </div>
+      <div className="section quote-section">
+        <p>
+          Collaboration and creative assistance result in gifting you more -
+          time that you can spend living your life, growing your business and
+          doing what you do time best.
+        </p>
+      </div>
+      <div className="section">
+        <p>img here</p>
+        <p>MEET MADELINE</p>
+        <button>
+          <a href="/about">About Me</a>
+        </button>
+      </div>
+      <Reviews />
+      <Footer />
     </>
   );
 }
--- CVA-Website/src/components/Hero.jsx
+++ CVA-Website/src/components/Hero.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import "./Hero.css";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <div className="poppyBG">
+        <h1>
+          Creative <br></br>Virtual<br></br>Assistance
+        </h1>
+        {/* flower image bg here */}
+        <div className="insetContent">
+          <p>
+            FOR ADVENTUROUS SOULS, TRAVEL FOCUSED AND WOMEN-OWNED BUSINESSES.
+          </p>
+          <p className="right-set-text">
+            FOR THE BIG <br></br>DREAMERS
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
